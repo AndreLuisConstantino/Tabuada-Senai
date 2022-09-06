be4832fff53becd82f5c142604dc34d31459092d
André Luiz <22282216@CFP127ALUNO.LOCAL>
2022-09-06 16:56:45 -0300

    Atualização

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,7 +1,23 @@
 package br.senai.sp.jandira;
 
+import br.senai.sp.jandira.model.Tabuada;
+
 public class AppTabuada {
+	
 	public static void main(String[] args) {
 		
+		 Tabuada Tabuada1  = new Tabuada();
+		 Tabuada1.multiplicando = 29;
+		 Tabuada1.minMultiplicando = 238;
+		 Tabuada1.maxMultiplicando = 2019;
+		
+		 
+		 String valorDaTabuada[] = Tabuada1.getTabuada();
+		 int contador = 0;
+		 
+		 while (contador < valorDaTabuada.length) {
+			 System.out.println(valorDaTabuada[contador]);
+			 contador++;
+		 }
 	}
 }
--- src/br/senai/sp/jandira/model/Tabuada.java
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -1,5 +1,36 @@
 package br.senai.sp.jandira.model;
 
+
+
 public class Tabuada {
 
+	//atributos
+	public int multiplicando;
+	public int minMultiplicando;
+	public int maxMultiplicando;
+	
+
+	
+	public String[] getTabuada() {
+		
+		int tamanho = maxMultiplicando - minMultiplicando + 1;
+		String[] tabuada = new String[tamanho];
+		
+		int contador = 0;
+		
+		while(contador < tamanho) {
+			
+		int valorDaTabuada = multiplicando * minMultiplicando;
+		
+		String resultado = multiplicando+ "X" +minMultiplicando +"="+ valorDaTabuada;
+		
+		tabuada[contador] = resultado;
+		
+		
+		contador++;
+		minMultiplicando++;
+	}
+		return tabuada;
+
+	}
 }
